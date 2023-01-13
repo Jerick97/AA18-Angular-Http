9ef192e51eda12271ca18960f7c46abd894c269f
Emerson Suarez <emersonsuarez2904@gmail.com>
2023-01-13 09:44:21 -0500

    Se consume Data

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  items!:string[]; //sera donde almacenemos los datos recibidos del servicio
+  constructor(private data:DataService) { } //private porque sera accesible solo aquí
 
   ngOnInit(): void {
+    this.data.getData().subscribe(data => this.items = data); //llamamos al método getData() y el contenido le pasamos al array Items
   }
 
+  editar(id:number){
+    console.log(id);
+  }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  private urlAPI = "https://jsonplaceholder.typicode.com/todos"; //url de nuestra API
 
-  constructor() { }
+  constructor(private http:HttpClient) { } //Importamos HttpClient
+
+  //Método que retorna la solicitud GET al API
+  getData():Observable<string[]>{
+    return this.http.get<string[]>(this.urlAPI);
+  }
 }
