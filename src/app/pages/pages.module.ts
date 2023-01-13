0a2cbc19f17ec59fc9710547002e9eb8f962d238
Emerson Suarez <emersonsuarez2904@gmail.com>
2023-01-13 09:24:51 -0500

    inicio del proyecto

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { DetalleComponent } from './detalle/detalle.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    DetalleComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HomeComponent,
+    DetalleComponent
+  ]
+})
+export class PagesModule { }
